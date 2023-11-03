1027dc463f9257a0747925377d6b38fbf2a8586c
Leonardo Nicolas <leonicolas@gmail.com>
2023-11-02 22:45:34 -0700

    Update map converter to generate multiple maps binary files

--- dev/map_converter.js
+++ dev/map_converter.js
@@ -1,63 +1,66 @@
 const fs = require("fs");
-const mapData = require("./map_stage1.json");
 
-const groundBuffer = Buffer.from(mapData.layers[0].data, 'base64');
-const solidBuffer = Buffer.from(mapData.layers[1].data, 'base64');
+function generateMapBinary(stage) {
+    const mapData = require(`./map_stage${stage}.json`);
 
-const mapBufferBytesPerTile = 2;
-const mapBuffer = Buffer.alloc(groundBuffer.length / mapBufferBytesPerTile);
+    const groundBuffer = Buffer.from(mapData.layers[0].data, 'base64');
+    const solidBuffer = Buffer.from(mapData.layers[1].data, 'base64');
 
-const mapCols=32;
-const tileSize=8;
+    const mapBufferBytesPerTile = 2;
+    const mapBuffer = Buffer.alloc(groundBuffer.length / mapBufferBytesPerTile);
 
-const objectsIds = {
-    blob: 1,
-    bat: 2,
-    "bat-wave": 3,
-};
+    const mapCols=32;
+    const tileSize=8;
 
-/**
- * Indexes objects by tile index
- */
-const tileObjects = mapData.layers[2].objects.reduce(
-    (data, object) => {
-        const higherX = Math.ceil(object.x);
-        const higherY = Math.ceil(object.y);
-        const normX = higherX - (higherX % tileSize);
-        const normY = higherY - (higherY % tileSize);
-        const index = (normY / tileSize) * mapCols + (normX / tileSize);
+    const objectsIds = {
+        blob: 1,
+        bat: 2,
+        "bat-wave": 3,
+    };
 
-        data[index] = Object.assign({}, object, { id: objectsIds[object.type] });
-        return data;
-    }, {}
-);
+    /**
+     * Indexes objects by tile index
+     */
+    const tileObjects = mapData.layers[2].objects.reduce(
+        (data, object) => {
+            const higherX = Math.ceil(object.x);
+            const higherY = Math.ceil(object.y);
+            const normX = higherX - (higherX % tileSize);
+            const normY = higherY - (higherY % tileSize);
+            const index = (normY / tileSize) * mapCols + (normX / tileSize);
 
-/**
- * 2 Bytes per tile
- * 0000 0000 0000 0000
- * |  |    | |--------> 1 byte: Tile Id
- * |  |    |----------> 1 bit: Solid bit
- * |  |---------------> 4 bits: Objects, enemies, power-ups
- * |------------------> 4 bits: Object properties
- */
-for (let i = 0; i < groundBuffer.length; i += 4) {
-    const groundValue = groundBuffer.readUInt32LE(i)
-    const solidValue = solidBuffer.readUInt32LE(i)
-    const objectData = tileObjects[i / 4];
+            data[index] = Object.assign({}, object, { id: objectsIds[object.type] });
+            return data;
+        }, {}
+    );
 
-    // Solid flag
-    let value = solidValue ? solidValue | 0x100 : groundValue;
-    // Object Id
-    if (objectData) {
-        value |= objectData.id << 9;
-        value |= getObjectPropertyValue(objectData) << 13;
-    }
-    mapBuffer.writeUInt16BE(value & 0xFFFF, i / mapBufferBytesPerTile);
-};
+    /**
+     * 2 Bytes per tile
+     * 0000 0000 0000 0000
+     * |  |    | |--------> 1 byte: Tile Id
+     * |  |    |----------> 1 bit: Solid bit
+     * |  |---------------> 4 bits: Objects, enemies, power-ups
+     * |------------------> 4 bits: Object properties
+     */
+    for (let i = 0; i < groundBuffer.length; i += 4) {
+        const groundValue = groundBuffer.readUInt32LE(i)
+        const solidValue = solidBuffer.readUInt32LE(i)
+        const objectData = tileObjects[i / 4];
 
-//fs.writeFileSync("ground.bin", groundBuffer);
-//fs.writeFileSync("solid.bin", solidBuffer);
-fs.writeFileSync("../maps/stage1.map", mapBuffer);
+        // Solid flag
+        let value = solidValue ? solidValue | 0x100 : groundValue;
+        // Object Id
+        if (objectData) {
+            value |= objectData.id << 9;
+            value |= getObjectPropertyValue(objectData) << 13;
+        }
+        mapBuffer.writeUInt16BE(value & 0xFFFF, i / mapBufferBytesPerTile);
+    };
+
+    //fs.writeFileSync("ground.bin", groundBuffer);
+    //fs.writeFileSync("solid.bin", solidBuffer);
+    fs.writeFileSync(`../maps/stage${stage}.map`, mapBuffer);
+}
 
 function getObjectPropertyValue(objectData) {
     let propertyValue = 0;
@@ -66,4 +69,8 @@ function getObjectPropertyValue(objectData) {
         return false;
     });
     return propertyValue > 0 ? propertyValue - 1 : 0;
+}
+
+for(let stage = 1; stage <= 2; ++stage) {
+    generateMapBinary(stage);
 }
