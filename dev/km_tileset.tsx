ca83a6253651b53fb29377c9cc523bc99a1cb416
Leonardo Nicolas <leonicolas@gmail.com>
2023-11-04 22:51:20 -0700

    Move km_tileset to maps tiles set

--- dev/km_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="km_tileset" tilewidth="8" tileheight="8" tilecount="960" columns="32" fillmode="preserve-aspect-fit">
- <image source="../tiles/km_tileset.png" width="256" height="240"/>
-</tileset>
